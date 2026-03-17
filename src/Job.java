58fa825af24fa946e55a7a3ea11f4ff2da523683
janeelliot <janeelliot@macbookair.lan>
2026-03-17 19:53:27 -0400

    testing!

---
+++ src/Job.java
@@ -0,0 +1,11 @@
+/** Preference file version 110624
+ * 
+ * @author: Your First and Last name goes here!
+ * VHS Learning
+ */
+
+public class Job
+{
+
+}
+ 
---
+++ src/Runner.java
@@ -0,0 +1,17 @@
+/**
+ * Preference file version 110624
+ * 
+ * @author: Your First and Last name goes here!
+ *          VHS Learning
+ */
+
+public class Runner
+{
+
+  public static void main(String[] args)
+  {
+    // TODO Auto-generated method stub
+    Player x = new Player();
+  }
+
+}
